1b49c705e0fbfd37835abf0481a77e50250239d5
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2024-10-24 09:52:49 +0530

    Remove Link from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './component/Login';
 import { ThemeContext } from './context/ThemeProvider';
